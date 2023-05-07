52b98c156d7f0b3d08df48c79264fdb981af58ba
Bereket Daniel <bereketdanny@gmail.com>
2023-05-07 00:48:14 -0400

    Product Service completed

---
+++ ProductService/src/main/java/com/example/productservice/model/Product.java
@@ -0,0 +1,19 @@
+package com.example.productservice.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private double price;
+
+
+}
